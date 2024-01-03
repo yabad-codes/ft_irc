336a456529e4f8c27d02bc692d953814201f2257
Yahya Abad <yabad@c1r5p8.1337.ma>
2024-01-03 19:06:17 +0100

    managing requests inside pollmanager

--- includes/PollManager.hpp
+++ includes/PollManager.hpp
@@ -6,7 +6,7 @@
 /*   By: yabad <yabad@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:40:11 by yabad             #+#    #+#             */
-/*   Updated: 2024/01/03 18:26:27 by yabad            ###   ########.fr       */
+/*   Updated: 2024/01/03 19:01:33 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,15 @@ struct server_info;
 
 class PollManager {
 	private:
-		void handle_new_connection(server_info*, std::unordered_map<int, User*>&, std::vector<struct pollfd>&);
-		void handle_client_activity(std::vector<struct pollfd>&, std::queue<Request*>&, std::unordered_map<int, User*>&, size_t);
-		void remove_disconnected_client(std::vector<struct pollfd>&, std::unordered_map<int, User*>&, size_t);
+		server_info* server;
+		std::unordered_map<int, User*>* users;
+		std::vector<struct pollfd>* pollfds;
+		std::queue<Request*>* requests;
+		void handle_new_connection();
+		void handle_client_activity(size_t);
+		void remove_disconnected_client(size_t);
+		void manage_requests();
+		Context* create_context_for_handler(Request*, User*);
 	public:
 		PollManager(server_info*, std::vector<struct pollfd>&, std::unordered_map<int, User*>&, std::queue<Request*>&);
 		~PollManager();
--- sources/PollManager.cpp
+++ sources/PollManager.cpp
@@ -6,13 +6,13 @@
 /*   By: yabad <yabad@student.1337.ma>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:46:17 by yabad             #+#    #+#             */
-/*   Updated: 2024/01/03 18:26:36 by yabad            ###   ########.fr       */
+/*   Updated: 2024/01/03 19:03:41 by yabad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PollManager.hpp"
 
-void PollManager::handle_new_connection(server_info* server, std::unordered_map<int, User*> &users, std::vector<struct pollfd> &pollfds) {
+void PollManager::handle_new_connection() {
 	struct pollfd client;
 	client.fd = accept(server->fd, NULL, NULL);
 	if (client.fd == -1) {
@@ -21,46 +21,75 @@ void PollManager::handle_new_connection(server_info* server, std::unordered_map<
 	}
 	User* new_user = new User(client.fd);
 	client.events = POLLIN;
-	pollfds.push_back(client);
-	users.insert(std::make_pair(client.fd, new_user));
+	this->pollfds->push_back(client);
+	this->users->insert(std::make_pair(client.fd, new_user));
 	std::cout << BOLD BRIGHT_BLUE << "[PollManager] " << RESET << "new connection from client with fd : " << client.fd << RESET <<std::endl;
 }
 
-void	PollManager::remove_disconnected_client(std::vector<struct pollfd> &pollfds, std::unordered_map<int, User*> &users, size_t index)
+void	PollManager::remove_disconnected_client(size_t index)
 {
-	int client_fd = pollfds[index].fd;
+	int client_fd = (*pollfds)[index].fd;
 	close(client_fd);
-	pollfds.erase(pollfds.begin() + index);
-	std::unordered_map<int, User*>::iterator it = users.find(client_fd);
-	if (it != users.end()) {
+	this->pollfds->erase(this->pollfds->begin() + index);
+	std::unordered_map<int, User*>::iterator it = this->users->find(client_fd);
+	if (it != this->users->end()) {
 		delete it->second;
-		users.erase(it);
+		this->users->erase(it);
 	}
 }
 
-void 	PollManager::handle_client_activity(std::vector<struct pollfd>& pollfds, std::queue<Request*> &requests, std::unordered_map<int, User*> &users, size_t index) {
+void 	PollManager::handle_client_activity(size_t index) {
 	char buffer[BUFSIZ];
 	int byte_read;
 
 	memset(buffer, '\0', BUFSIZ);
-	byte_read = recv(pollfds[index].fd, buffer, BUFSIZ, 0);
+	byte_read = recv((*pollfds)[index].fd, buffer, BUFSIZ, 0);
 	if (byte_read == -1)
-		std::cout << BOLD BRIGHT_YELLOW << "[PollManager] " << RESET << "error while receiving data from client with fd : " << pollfds[index].fd << RESET << std::endl;
+		std::cout << BOLD BRIGHT_YELLOW << "[PollManager] " << RESET << "error while receiving data from client with fd : " << (*pollfds)[index].fd << RESET << std::endl;
 	else if (byte_read == 0)
 	{
-		std::cout << BOLD BRIGHT_YELLOW << "[PollManager] " << RESET << "client with fd : " << pollfds[index].fd <<" disconnected" << RESET << std::endl;
-		remove_disconnected_client(pollfds, users, index);
+		std::cout << BOLD BRIGHT_YELLOW << "[PollManager] " << RESET << "client with fd : " << (*pollfds)[index].fd <<" disconnected" << RESET << std::endl;
+		remove_disconnected_client(index);
 	}
 	else
-		Parser irc_parser(requests, buffer, pollfds[index].fd);
+		Parser irc_parser(*requests, buffer, (*pollfds)[index].fd);
+}
+
+Context* PollManager::create_context_for_handler(Request* req, User* user) {
+	Context* context = new Context;
+	context->request = req;
+	context->user = user;
+	return context;
+}
+
+void PollManager::manage_requests() {
+	int index = 0;
+	while (!this->requests->empty() && index < NREQUESTSTOHANDLE) {
+		Request* request = this->requests->front();
+		RequestHandler handler;
+		try {
+			Context* context = create_context_for_handler(request, this->users->find(request->get_fd())->second);
+			handler.handle_request(context);
+			delete context;
+		} catch (std::exception& e) {
+			//handle exception
+		}
+		this->requests->pop();
+		delete request;
+		index++;
+	}
 }
 
 PollManager::PollManager(server_info* server, std::vector<struct pollfd>& pollfds, std::unordered_map<int, User*>& users, std::queue<Request*>& requests) {
-	pollfds.push_back(pollfd());
+	this->server = server;
+	this->users = &users;
+	this->pollfds = &pollfds;
+	this->requests = &requests;
+	this->pollfds->push_back(pollfd());
 	pollfds[0].fd = server->fd;
 	pollfds[0].events = POLLIN;
 	while (true) {
-		if (poll(&pollfds[0], pollfds.size(), TIMEOUT) == -1) {
+		if (poll(&pollfds[0], this->pollfds->size(), TIMEOUT) == -1) {
 			if (errno == EINTR) {
 				std::cout << BOLD BRIGHT_YELLOW << "[PollManager] " << RESET << "poll has been interrupted by a signal" << RESET << std::endl;
 				continue;
@@ -69,14 +98,14 @@ PollManager::PollManager(server_info* server, std::vector<struct pollfd>& pollfd
 		}
 
 		if (pollfds[0].revents & POLLIN)
-			handle_new_connection(server, users, pollfds);
+			handle_new_connection();
 
-		for (size_t i = 1; i < pollfds.size(); i++) {
+		for (size_t i = 1; i < this->pollfds->size(); i++) {
 			if (pollfds[i].revents & POLLIN)
-				handle_client_activity(pollfds, requests, users, i);
+				handle_client_activity(i);
 		}
 
-		//handling requests here
+		manage_requests();
 	}
 }
 
