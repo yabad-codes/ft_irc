e79d095ef893f1d06f8500ef95e1969a751ae2f0
Hassna Ouattou <houattou@c1r5p7.1337.ma>
2024-01-14 13:22:55 +0100

    create channel, join channel, handle response for create channel and join to exists channel

--- sources/WhoCmd.cpp
+++ sources/WhoCmd.cpp
@@ -1,94 +1,168 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   WhoCmd.cpp                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: houattou <houattou@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/05 17:45:31 by houattou          #+#    #+#             */
-/*   Updated: 2024/01/09 13:14:16 by houattou         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+// /* ************************************************************************** */
+// /*                                                                            */
+// /*                                                        :::      ::::::::   */
+// /*   WhoCmd.cpp                                        :+:      :+:    :+:   */
+// /*                                                    +:+ +:+         +:+     */
+// /*   By: houattou <houattou@student.42.fr>          +#+  +:+       +#+        */
+// /*                                                +#+#+#+#+#+   +#+           */
+// /*   Created: 2024/01/05 17:45:31 by houattou          #+#    #+#             */
+// /*   Updated: 2024/01/09 13:14:16 by houattou         ###   ########.fr       */
+// /*                                                                            */
+// /* ************************************************************************** */
 
-# include "ICommand.hpp"
-#include <set>
-std::string WhoCmd::convert_name_channel_to_lowercase(std::string name_channel)
-{
-      std::string channel_name = name_channel;
-        for (size_t i = 0; i < channel_name.length(); ++i) 
-            channel_name[i] = std::tolower(channel_name[i]);
-        return(channel_name);    
-    
-}
+// # include "ICommand.hpp"
+// #include <set>
 
-void WhoCmd::execute(Context *context)
-{
-    std::string name_channel = context->request->get_options();
-    std::cout << "name channel is " << name_channel << std::endl;
-    generate_response(context);
-}
-int WhoCmd::get_type()
-{
-    return(type);
-}
+// std::string convert_name_channel_to_lowercase(std::string name_channel);
+// class WhoCmd : public ICommand 
+// {
+// 	private:
+//         response_type type;
+// 		bool is_operator(User *user,Context *context);
+//         int get_type();
 
-// >> :Osmium.AfterNET.Org 353 non = #afet :non!yes@88ABE6.25BF1D.D03F86.88C9BD.IP @abad!yahia@88ABE6.25BF1D.D03F86.88C9BD.IP
-std::string WhoCmd::get_all_users_in_channel(Context *context)
-{
-    std::string admin;
-    std::string name_channel = convert_name_channel_to_lowercase(context->request->get_options());
-    User *user = context->users->find(context->request->get_fd())->second;
-    std::string members;
-    members += ":myserver 353 "+ user->get_nickname() +" = " + context->request->get_options()+" :@"+ user->get_nickname()+"!"+user->get_username()+ "@"+user->get_hostname() ;
-    std::map<std::string, Channel *>::iterator it = context->channels->find(name_channel);
-    if(it != context->channels->end())
-    {
-        Channel *channel = it->second;
-        std::vector<User *>users = channel->get_users();
-        for(size_t i = 0; i < users.size(); i++)
-        {
-            if(user->get_nickname() != users[i]->get_nickname())
-                members += " @"+users[i]->get_nickname()+"!"+users[i]->get_username()+ "@"+ users[i]->get_hostname() + " ";
-            std::cout << users[i]->get_username() << std::endl;    
-        }
-       members += "\r\n";
-    }
-        return(members);
-}
+// 		std::string  user_status_prefix(User *user, Context *context);
 
+// 		std::string append_users_to_members_string(Context *context, Channel *channel, User *user, std::string members);
+// 		std::string create_initial_members_string(Context *context, User *user,std::string user_status_prefix);
+// 		std::string create_members_string(Context *context, User *user, std::string admin);
+// 		std::string reply_names(Context *);
+// 		// std::string get_users_in_channel(Context *context, std::string name_channel, std::string members, User *user, std::string admin);
+// 		std::map<std::string, Channel*> ::iterator is_exist_channel(Context *context);
+// 		std::string reply_end_of_names(User *user, std::string name_channel);
+// 		std::string reply_channel_mode_is(User *user, std::string name_channel);
+// 		Response* response;
+// 	public:
+// 		void execute(Context*);
+// 		void generate_response(Context*);
+// };
+// std::string convert_name_channel_to_lowercase(std::string name_channel)
+// {
+//       std::string channel_name = name_channel;
+//         for (size_t i = 0; i < channel_name.length(); ++i) 
+//             channel_name[i] = std::tolower(channel_name[i]);
+//         return(channel_name);
+// }
 
-void WhoCmd::generate_response(Context*context)
-{
-    std::string admin;
-   std::string name_channel = convert_name_channel_to_lowercase(context->request->get_options());
-    std::string res;
-    Request *req = context->request;
-    User *user = context->users->find(context->request->get_fd())->second;
-    std::map<std::string, Channel *>::iterator it = context->channels->find(name_channel);
-    std::string all_users_in_channel = get_all_users_in_channel(context);
-    res += all_users_in_channel;
-    res += ":myserver 366 " + user->get_nickname() + " " + context->request->get_options() + ":End of NAMES list\r\n";
-    res += ":myserver 324 " + user->get_nickname() + context->request->get_options() + " +\r\n";
-   if(it != context->channels->end())
-   {
-        Channel *channel = it->second;
-        std::vector<User*> users = channel->get_users();
-        std::set<std::string> operators = channel->get_operators();
+// int WhoCmd::get_type()
+// {
+//     return(type);
+// }
+// std::map<std::string, Channel*> ::iterator WhoCmd::is_exist_channel(Context *context)
+// {
+//     std::string name_channel = convert_name_channel_to_lowercase(context->request->get_options());
+//     std::map<std::string, Channel*> *  channel  = context->channels;
+//     std::map<std::string, Channel *>::iterator it = channel->find(name_channel);
+//     if(it != channel->end())
+//         return(it);
+//     return(channel->end());    
+// }
 
-        for(size_t i = 0; i < users.size(); i++)
-        {
-            std::set<std::string>::iterator it_set = operators.find(users[i]->get_nickname());
-            if(it_set != operators.end())
-                admin = "H@x 0 :" + users[i]->get_realname() + "\r\n";
-            else 
-                admin = "Hx 0 :" + users[i]->get_realname() + "\r\n"; 
-            res += ":myserver 354 " + user->get_nickname() + " 152 " +  context->request->get_options() + " "+ users[i]->get_username() + " " + users[i]->get_hostname() + " "+users[i]->get_nickname() + " " + admin;
-            // res += ":myserver 352 " + user->get_nickname() + " " + req->get_options() + " " +user->get_username() + " "+ user->get_hostname() + " " + users[i]->get_nickname() + " " + admin ;
-        }
-     }
-     //>> :Osmium.AfterNET.Org 366 hasona #gg :End of /NAMES list.
-    res +=":myserver 315 " + user->get_nickname() + " " + context->request->get_options() + " :End of /WHO list." + "\r\n";
-    this->response = new Response(res);
-    user->add_response(this->response);
+// bool WhoCmd::is_operator(User *user, Context *context)
+// {
+//     std::map<std::string, Channel *> ::iterator it =is_exist_channel(context);
+//     if(it != context->channels->end())
+//     {
+//         Channel *channel = it->second;
+//         std::set<std::string>operators = channel->get_operators();
+//         std::set<std::string>::iterator it_set = operators.find(user->get_nickname());
+//         if(it_set != operators.end())
+//             return(true);
+//     }
+//     return(false);
+// }
+// std::string WhoCmd:: user_status_prefix(User *user, Context *context )
+// {
+//     std::string user_status;
+//     if(is_operator(user,context))
+//         user_status = "@";
+//     else
+//         user_status= "";
+//     return(user_status);  
 
-}
+// }
+// std::string WhoCmd::create_initial_members_string(Context *context, User *user,std::string user_status_prefix)
+// {
+//     std::string members;
+//     members += ":myserver 353 "+ user->get_nickname() +" = " + context->request->get_options()+ " :"+ \
+//     user_status_prefix + user->get_nickname() + " ";
+//     return(members);
+// }
+
+// std::string WhoCmd::append_users_to_members_string(Context *context, Channel *channel, User *user, std::string members)
+// {
+//     std::vector<User *>users = channel->get_users();
+//     for(size_t i = 0; i < users.size(); i++)
+//     {
+//         if(user->get_nickname() != users[i]->get_nickname())
+//             members += user_status_prefix(users[i], context) + users[i]-> get_nickname() + " ";      
+//     }
+//     members += "\r\n";
+//     return(members);
+// }
+// std::string WhoCmd::reply_names(Context *context)
+// {
+//     User *user = context->users->find(context->request->get_fd())->second;
+//     std::string members = create_initial_members_string(context, user, user_status_prefix(user, context));
+//     std::map<std::string, Channel * > ::iterator it = is_exist_channel(context);
+//     if(it != context->channels->end())
+//     {
+//         Channel *channel = it->second;
+//         members = append_users_to_members_string(context,channel,user,members);
+//     }
+//     return(members);
+// }
+
+// void WhoCmd::execute(Context *context)
+// {
+//     generate_response(context);
+// }
+
+// std::string WhoCmd::reply_end_of_names(User *user, std::string name_channel)
+// {
+//     std::string res;
+//      res = ":myserver 366 " + user->get_nickname();
+//      res += " " +name_channel;
+//      res += ":End of NAMES list\r\n";
+//      return(res);
+// }
+// std::string WhoCmd::reply_channel_mode_is(User *user, std::string name_channel)
+// {
+//     std::string res;
+//     res = ":myserver 324 " + user->get_nickname() + " "+ name_channel + " +\r\n";
+//     return(res);
+// }
+// void WhoCmd::generate_response(Context*context)
+// {
+//     (void)context;
+  
+//     // std::string admin;
+// //    std::string name_channel = (context->request->get_options());
+// //     std::string res;
+// //     Request *req = context->request;
+// //     User *user = context->users->find(context->request->get_fd())->second;
+// //     // std::map<std::string, Channel *>::iterator it = context->channels->find(name_channel);
+// //     res = reply_names(context);
+// //     // res += all_users_in_channel;
+// //     res += reply_end_of_names(user,name_channel);
+// //     res += reply_channel_mode_is(user, name_channel);
+// //     this->response = new Response(res);
+// //     user->add_response(this->response);
+// //    if(it != context->channels->end())
+// //    {
+// //         Channel *channel = it->second;
+// //         std::vector<User*> users = channel->get_users();
+// //         std::set<std::string> operators = channel->get_operators();
+
+// //         for(size_t i = 0; i < users.size(); i++)
+// //         {
+// //             std::set<std::string>::iterator it_set = operators.find(users[i]->get_nickname());
+// //             if(it_set != operators.end())
+// //                 admin = "H@x 0 :" + users[i]->get_realname() + "\r\n";
+// //             else 
+// //                 admin = "Hx 0 :" + users[i]->get_realname() + "\r\n"; 
+// //             res += ":myserver 354 " + user->get_nickname() + " 152 " +  context->request->get_options() + " "+ users[i]->get_username() + " " + users[i]->get_hostname() + " "+users[i]->get_nickname() + " " + admin;
+// //         }
+// //      }
+// //     res +=":myserver 315 " + user->get_nickname() + " " + context->request->get_options() + " :End of /WHO list." + "\r\n";
+// }
